8a3a8b02ea5572e1d6398ed36f501ee31d005bda
Sofía <sofiavillarejo.dev@gmail.com>
2025-03-24 13:50:37 +0100

    Add project files.

---
+++ ApiCoreCrudDepartamentos/Controllers/DepartamentoController.cs
@@ -0,0 +1,74 @@
+﻿using ApiCoreCrudDepartamentos.Models;
+using ApiCoreCrudDepartamentos.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCoreCrudDepartamentos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartamentoController : ControllerBase
+    {
+        private RepositoryDepartamentos repo;
+
+        public DepartamentoController(RepositoryDepartamentos repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Departamento>>> GetDepartamentos()
+        {
+            return await this.repo.GetDepartamentosAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Departamento>> FindDepartamento(int id)
+        {
+            return await this.repo.FindDepartamentoAsync(id);
+        }
+
+        //LOS METODOS POR DEFECTO DE POST O PUT RECIBEN UN OBJETO
+        //SI QUEREMOS ENVIAR PARAMETROS DBEMOS MAPEARLOS CON ROUTING
+        [HttpPost]
+        public async Task<ActionResult> InsertDepartamento(Departamento dept) //recibe el objeto directamente y por debajo, lo transforma a json
+        {
+            await this.repo.InsertDepartamento(dept.IdDepartamento, dept.Nombre, dept.Localidad);
+            return Ok();
+        }
+
+        //EL METODO POR DEFECTO DE DELETE RECIBE UN ID
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteDepartamento(int id)
+        {
+            await this.repo.DeleteDepartamento(id);
+            return Ok();
+        }
+
+        public async Task<ActionResult> UpdateDepartamento(Departamento dept)
+        {
+            await this.repo.UpdateDepartamento(dept.IdDepartamento, dept.Nombre, dept.Localidad);
+            return Ok();
+        }
+
+        //PODEMOS PERSONALIZAR METODOS POST, PUT O DELETE
+        //EJ -> POST CON PARAMETROS
+
+        [HttpPost]
+        [Route("[action]/{id}/{nombre}/{localidad}")]
+        public async Task<ActionResult> PostDepartamento(int id, string nombre, string localidad)
+        {
+            await this.repo.InsertDepartamento(id, nombre, localidad);
+            return Ok();
+        }
+
+        //SI LO NECESITAMOS, PODEMOS COMBINAR OBJETOS CON PARAMETROS -> EL OBJETO ES EL ÚLTIMO ELEMENTO QUE SE INCLUYE EN LA PETICION DEL METODO
+        [HttpPost]
+        [Route("[action]/{id}")]
+        public async Task<ActionResult> PutDepartamento(int id, Departamento departamento)
+        {
+            await this.repo.UpdateDepartamento(id, departamento.Nombre, departamento.Localidad);
+            return Ok();
+        }
+    }
+}
---
+++ ApiCoreCrudDepartamentos/Repositories/RepositoryDepartamentos.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.CompilerServices;
+using ApiCoreCrudDepartamentos.Data;
+using ApiCoreCrudDepartamentos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCoreCrudDepartamentos.Repositories
+{
+    public class RepositoryDepartamentos
+    {
+        private DepartamentosContext context;
+
+        public RepositoryDepartamentos(DepartamentosContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Departamento>> GetDepartamentosAsync()
+        {
+            return await this.context.Departamentos.ToListAsync();
+        }
+
+        public async Task<Departamento> FindDepartamentoAsync(int idDepartamento)
+        {
+            return await this.context.Departamentos.FirstOrDefaultAsync(x => x.IdDepartamento == idDepartamento);
+        }
+
+        public async Task InsertDepartamento(int id, string nombre, string localidad)
+        {
+            Departamento dept = new Departamento();
+            dept.IdDepartamento = id;
+            dept.Nombre = nombre;
+            dept.Localidad = localidad;
+            await this.context.Departamentos.AddAsync(dept);
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task UpdateDepartamento(int id, string nombre, string localidad)
+        {
+            Departamento dept = await this.FindDepartamentoAsync(id);
+            dept.Nombre = nombre;
+            dept.Localidad = localidad;
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task DeleteDepartamento(int id)
+        {
+            Departamento dept = await this.FindDepartamentoAsync(id);
+            this.context.Departamentos.Remove(dept);
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
